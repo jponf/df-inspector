bf246dd84249fd30d3d80d7fbd6fa06a1d5e31db
Eduard Torres <etorres404@gmail.com>
2017-07-12 11:46:04 +0200

    Implementation of FunctionMultiply unittests

--- tests/test_primitives_arithmetic.py
+++ tests/test_primitives_arithmetic.py
@@ -4,7 +4,7 @@ from csvinspector.lang import listops
 from csvinspector.lang.environment import NestedEnvironment
 from csvinspector.lang.symbol import SYM_NIL
 from csvinspector.lang.types import Integer
-from csvinspector.primitives import FunctionAdd, FunctionSubtract
+from csvinspector.primitives import FunctionAdd, FunctionSubtract, FunctionMultiply
 
 
 class TestFunctionAdd(unittest.TestCase):
@@ -59,3 +59,30 @@ class TestFunctionSubtract(unittest.TestCase):
         op = listops.from_list([Integer(-2), Integer(-3)])
         result = self.f_subtract.apply(op, self.env)
         self.assertEqual(result, Integer(1))
+
+
+class TestFunctionMultiply(unittest.TestCase):
+
+    def setUp(self):
+        self.env = NestedEnvironment()
+        self.f_multiply = FunctionMultiply('*')
+
+    def test_base_multiply(self):
+        result = self.f_multiply.apply(SYM_NIL, self.env)
+        self.assertEqual(result, Integer(1))
+
+    def test_multiplication_of_two_integers(self):
+        op = listops.from_list([Integer(2), Integer(3)])
+        result = self.f_multiply.apply(op, self.env)
+        self.assertEqual(result, Integer(6))
+
+    def test_multiplication_of_more_integers(self):
+        op = listops.from_list([Integer(1), Integer(2),
+                                Integer(3), Integer(4)])
+        result = self.f_multiply.apply(op, self.env)
+        self.assertEqual(result, Integer(24))
+
+    def test_multiplication_of_negative_numbers(self):
+        op = listops.from_list([Integer(-2), Integer(3)])
+        result = self.f_multiply.apply(op, self.env)
+        self.assertEqual(result, Integer(-6))
