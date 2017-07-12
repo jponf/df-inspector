92b5af9a7a4e97765f71f274324e9752481ad95d
Eduard Torres <etorres404@gmail.com>
2017-07-12 11:56:18 +0200

    Implementation of FunctionDivide unittests

--- tests/test_primitives_arithmetic.py
+++ tests/test_primitives_arithmetic.py
@@ -3,8 +3,9 @@ import unittest
 from csvinspector.lang import listops
 from csvinspector.lang.environment import NestedEnvironment
 from csvinspector.lang.symbol import SYM_NIL
-from csvinspector.lang.types import Integer
-from csvinspector.primitives import FunctionAdd, FunctionSubtract, FunctionMultiply
+from csvinspector.lang.types import Integer, Real
+from csvinspector.primitives import FunctionAdd, FunctionSubtract, \
+    FunctionMultiply, FunctionDivide
 
 
 class TestFunctionAdd(unittest.TestCase):
@@ -86,3 +87,34 @@ class TestFunctionMultiply(unittest.TestCase):
         op = listops.from_list([Integer(-2), Integer(3)])
         result = self.f_multiply.apply(op, self.env)
         self.assertEqual(result, Integer(-6))
+
+
+class TestFunctionDivide(unittest.TestCase):
+
+    def setUp(self):
+        self.env = NestedEnvironment()
+        self.f_divide = FunctionDivide('/')
+
+    def test_base_divide(self):
+        result = self.f_divide.apply(SYM_NIL, self.env)
+        self.assertEqual(result, Integer(1))
+
+    @unittest.expectedFailure
+    def test_division_by_0(self):
+        op = listops.from_list([Integer(10), Integer(0)])
+        self.f_divide.apply(op, self.env)
+
+    def test_exact_integer_division(self):
+        op = listops.from_list([Integer(10), Integer(2)])
+        result = self.f_divide.apply(op, self.env)
+        self.assertEqual(result, Integer(5))
+
+    def test_approximate_integer_division(self):
+        op = listops.from_list([Integer(10), Integer(3)])
+        result = self.f_divide.apply(op, self.env)
+        self.assertEqual(result, Integer(3))
+
+    def test_real_division(self):
+        op = listops.from_list([Real(3.0), Integer(2)])
+        result = self.f_divide.apply(op, self.env)
+        self.assertEqual(result, Real(1.5))
